fc4ee04198d5abbe4e194f051890748bd753b59e
stalg94 <stalg28@gmail.com>
2022-01-17 10:54:03 +0200

    init

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('title',$post['title'])
+
+
+
+@section('content')
+@if ($post['is_new'])
+    <div>A new Blog post! Using if</div>
+@elseif (!$post['is_new'])
+    <div>Blog post is old!</div>
+@endif
+
+
+@unless ($post['is_new'])
+    <div>It is an old post... using unless</div>
+@endunless
+
+<h1>{{$post['title']}}</h1>
+<p>{{$post['content']}}</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('home.index');
+// })->name('home.index');
+
+// Route::get('/contact', function () {
+//     return 'home.contact';
+// })->name('home.contact');
+
+Route::get('/post/{id}', function ($id) {
+    $posts=[
+        1 =>[
+            'title' => 'Intro to Laravel',
+            'content' => 'This is a short intro to Laravel',
+            'is_new'=> true,
+        ],
+        2 => [
+            'title' => 'Intro to PHP',
+            'content' => 'This is a short intro to PHP',
+            'is_new'=> false,
+
+
+        ]
+    ];
+
+    abort_if(!isset($posts[$id]), 404);
+
+    return view('posts.show', ['post' => $posts[$id]]);
+})
+// ->where([
+//     'id' => '[0-9]+'
+// ])
+->name('posts.show');
+
+Route::view('/', 'home.index')->name('home.index');
+Route::view('/contact', 'home.contact')->name('home.contact');
+
+Route::get('recent-posts/{days_ago?}',function($days_ago = 20){
+    return 'Posts from '.$days_ago . ' days ago';
+})->name('posts.recent.index');
+
+
